706b644fbe7d4550ea4a07c9179d6c6b8cfc8f73
Bogdan Savenko <savenko.bogdan@gmail.com>
2026-07-08 14:05:01 +0200

    expose facts db to blueprints

--- Source/DoomsdayDevice/Public/Flow/FactsDBSubsystem.h
+++ Source/DoomsdayDevice/Public/Flow/FactsDBSubsystem.h
@@ -17,11 +17,19 @@ class DOOMSDAYDEVICE_API UFactsDBSubsystem : public UGameInstanceSubsystem
 	GENERATED_BODY()
 	
 public:
+	UFUNCTION(BlueprintCallable)
 	void AddFact(const FGameplayTag& Tag, const int32 Value);
+
+	UFUNCTION(BlueprintCallable)
 	void IncrementFact(const FGameplayTag& Tag, const int32 Value);
+
+	UFUNCTION(BlueprintCallable)
 	void RemoveFact(const FGameplayTag& Tag);
 
+	UFUNCTION(BlueprintCallable)
 	bool DoesFactExist(const FGameplayTag& Tag) const;
+
+	UFUNCTION(BlueprintCallable)
 	int32 GetFactValue(const FGameplayTag& Tag) const;
 
 private:
